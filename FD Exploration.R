5f2ddb9c040dc94a097454589803783cdb5c3c68
Seth Goldman <segoldma@github.com>
2018-04-28 10:19:50 -0400

    Lazy stackability ranking

--- FD Exploration.R
+++ FD Exploration.R
@@ -2,27 +2,35 @@ library(dplyr)
 library(readr)
 
 # Read in SaberSim data
-ss_batters <- read_csv("ss batters.csv")
-ss_pitchers <- read.csv("ss pitchers.csv", fileEncoding = "UTF-8-BOM")
+ss_batters <- read_csv("FanGraphs Leaderboard (5).csv") %>%  
+  filter(!is.na(Game))
+ss_pitchers <- read.csv("FanGraphs Leaderboard (6).csv", fileEncoding = "UTF-8-BOM") %>% 
+  filter(!is.na(Game))
 
 # Read in sample FanDuel player list
-fd <- read_csv("FanDuel-MLB-2018-04-20-25194-players-list.csv")
+fd <- read_csv("FanDuel-MLB-2018-04-28-25410-players-list.csv")
 
-# Try to match on names
-fd_sample <- fd %>% 
-  select(Nickname, FPPG)
+# Teams with the most projected points (full roster)
+batters %>% 
+  group_by(Team) %>% 
+  summarise("xpts" = sum(FanDuel)) %>% 
+  arrange(desc(xpts))
+  
+# View Top four batters by team
+  batters %>% 
+    arrange(desc(FanDuel)) %>% 
+    group_by(Team) %>% 
+    slice(1:4) %>% View("top four batters by team")
+  
+# Stackability ranking
+  batters %>% 
+    arrange(desc(FanDuel)) %>% 
+    group_by(Team) %>% 
+    slice(1:4) %>% 
+    group_by(Team) %>% 
+    summarise("xspts" = sum(FanDuel)) %>% 
+    arrange(desc(xspts))
 
-ss_batters %>% 
-  select(Name, Team) %>% 
-  left_join(fd_sample, by = c("Name" = "Nickname")) %>% 
-  group_by(is.na(FPPG)) %>% 
-  tally()
-
-ss_pitchers %>% 
-  select(Name, Team) %>% 
-  left_join(fd_sample, by = c("Name" = "Nickname")) %>% 
-  group_by(is.na(FPPG)) %>% 
-  tally()
 
 # Points per dollar
 ss_pitchers %>% 
